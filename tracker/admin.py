8a5e18f9864e14d031a9384dd1d9101d78299d0f
Gordon-Coder <gordonpeng27@gmail.com>
2026-03-05 22:26:24 +0800

    Registered models into admin

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Event, Competitor, Throw
 
 # Register your models here.
+admin.site.register(Event)
+admin.site.register(Competitor)
+admin.site.register(Throw)
