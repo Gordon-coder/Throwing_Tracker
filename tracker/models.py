0e92286aade56c97e0b1e18d05542e5e0996a076
Gordon-Coder <gordonpeng27@gmail.com>
2026-03-05 22:17:05 +0800

    added models

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Competitor(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Throw(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    competitor = models.ForeignKey(Competitor, on_delete=models.CASCADE)
+    distance = models.FloatField()
+
+    def __str__(self):
+        return f"{self.competitor.name} - {self.event.name}: {self.distance}m"
